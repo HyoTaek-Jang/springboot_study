a02dfd0225129df4081e5faa385e1039e07c1e52
HYOTAEK <hyotaek9812@gmail.com>
2021-06-26 20:31:32 +0900

    jdbc, h3 디비연결

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -22,12 +25,12 @@ public class MemberController {
 
 
     @GetMapping("/members/new")
-    public String createForm(){
+    public String createForm() {
         return "members/createMemberForm";
     }
 
     @PostMapping("/members/new")
-    public String create(MemberForm form){
+    public String create(MemberForm form) {
 //        바디를 받아와서 form에 맞게 set함.
         Member member = new Member();
         member.setName(form.getName());
@@ -37,5 +40,12 @@ public class MemberController {
         return "redirect:/";
 //        홈 화면으로 돌리기
 
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }

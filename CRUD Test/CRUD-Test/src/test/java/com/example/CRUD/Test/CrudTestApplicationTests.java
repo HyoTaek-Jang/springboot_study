d7acfb84a47db9de92d29a6c13972067631d1fd1
HYOTAEK <hyotaek9812@gmail.com>
2021-06-30 12:23:49 +0900

    test code 작성

--- CRUD Test/CRUD-Test/src/test/java/com/example/CRUD/Test/CrudTestApplicationTests.java
+++ CRUD Test/CRUD-Test/src/test/java/com/example/CRUD/Test/CrudTestApplicationTests.java
@@ -2,12 +2,22 @@ package com.example.CRUD.Test;
 
 import com.example.CRUD.Test.domain.User;
 import com.example.CRUD.Test.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
-class CrudTestApplicationTests{
+@Slf4j
+@SpringBootTest
+@Transactional
+class CrudTestApplicationTests {
 
+    //    @Autowired
     private UserRepository userRepository;
 
     @Autowired
@@ -29,5 +39,47 @@ class CrudTestApplicationTests{
         System.out.println(newUser.toString());
 
     }
+
+    @Test
+    public void read() {
+        Optional<User> user = userRepository.findById(2L);
+        user.ifPresent(selectUser -> { // ifPresent: 값이 있다면, 가져옴
+            log.info(selectUser.toString());
+        });
+    }
+
+    @Test
+    public void update() {
+        Optional<User> user = userRepository.findById(2L);
+        user.ifPresent(selectUser -> {
+            User newUser = User.builder()
+                    .id(selectUser.getId())
+                    .account(selectUser.getAccount())
+                    .phoneNumber(selectUser.getPhoneNumber())
+                    .email("updatedEmail@google.com")
+                    .createdBy(selectUser.getCreatedBy())
+                    .updatedAt(LocalDateTime.now())
+                    .updatedBy("TestUser02")
+                    .build();
+            userRepository.save(newUser);
+            log.info(newUser.toString());
+
+        });
+    }
+
+    @Test
+    public void delete() {
+        Optional<User> user = userRepository.findById(1L);
+        // User가 있어야 함
+        Assertions.assertTrue(user.isPresent());
+        // 있다면 삭제
+        user.ifPresent(selectUser -> {
+            userRepository.deleteById(selectUser.getId());
+        });
+        // 지워졌는지 확인
+        Optional<User> deletedUser = userRepository.findById(1L);
+        Assertions.assertFalse(deletedUser.isPresent());
+    }
+
 
 }

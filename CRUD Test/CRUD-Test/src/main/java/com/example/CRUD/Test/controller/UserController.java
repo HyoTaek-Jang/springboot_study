dc002e9fb8bce3a788838dfddfde19109728ba99
HYOTAEK <hyotaek9812@gmail.com>
2021-07-03 18:52:19 +0900

    add new Project for 스프링핵심원리

--- CRUD Test/CRUD-Test/src/main/java/com/example/CRUD/Test/controller/UserController.java
+++ CRUD Test/CRUD-Test/src/main/java/com/example/CRUD/Test/controller/UserController.java
@@ -1,33 +1,54 @@
 package com.example.CRUD.Test.controller;
 
 import com.example.CRUD.Test.domain.User;
+import com.example.CRUD.Test.repository.UserRepository;
 import com.example.CRUD.Test.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Member;
+import java.util.List;
+import java.util.Optional;
 
-@RestController
+@RestController()
+@RequestMapping("/api/*")
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
-    @RequestMapping(value = "/setUser")
-    public String setUser(User user) throws Exception{
+
+    @PostMapping(value = "/setUser")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public User setUser(@RequestBody User user) throws Exception {
         System.out.println(user.toString());
-        return "set";
+        User save = userRepository.save(user);
+        return save;
     }
 
     @GetMapping("/get")
-    public String hello(){
-        return "hello";
+    public List<User> hello() {
+        List<User> result = userRepository.findAll();
+        return result;
+    }
+
+    @GetMapping("/getById/{id}")
+    public Optional<User> getById(@PathVariable Long id){
+        Optional<User> byId = userRepository.findById(id);
+        return byId;
+    }
+
+    @GetMapping("/getById")
+    public Optional<User> getById2(@RequestParam Long id){
+        Optional<User> byId = userRepository.findById(id);
+        return byId;
     }
 }
